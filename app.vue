2f75c20c5a173da3e05a2dedbaa31c2f5957881f
lizongxiao <158638447@qq.com>
2024-06-14 16:00:55 +0800

    feat:组件库vant的使用

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Chiffon Blog",
+  description: "一个只想学习的前端博客",
+});
+</script>
+
 <template>
   <NuxtLayout>
     <nuxt-link to="/">首页</nuxt-link>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true, // 默认开启服务端渲染
-  devtools: { enabled: true },
+  devtools: { enabled: true },// 开启调试工具
+  modules: ['@vant/nuxt'],
 });
